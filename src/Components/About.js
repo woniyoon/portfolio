76a8cc6a10c7a0f258e53cdce2cf274c363214b4
woniyoon <gellifisch@gmail.com>
2020-10-07 23:59:01 +0900

    Create About component

---
+++ src/Components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./About.css";
+
+function About(){
+    return (
+        <div id="about" className="about">
+            자기 소개 페이지 ^^
+            
+        </div>
+    );
+}
+
+export default About;
