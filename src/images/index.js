f8c8f01de667a5a771cfbc938254fd7b22f7b16c
woniyoon <gellifisch@gmail.com>
2020-10-07 23:55:02 +0900

    Add image resources

---
+++ src/images/index.js
@@ -0,0 +1,11 @@
+export const sbScreen1 = require("./sbScreenshot1.png");
+export const sbScreen2 = require("./sbScreenshot2.png");
+export const sbScreen3 = require("./sbScreenshot3.png");
+export const sdScreen1 = require("./sdScreen1.png");
+export const sdScreen2 = require("./sdScreen2.png");
+export const sdScreen3 = require("./sdScreen3.png");
+export const sdScreen4 = require("./sdScreen4.png");
+export const socdocLogo = require("./socdocLogoT.png");
+export const sbWeb = require("../images/sbWeb.png");
+export const sdArch = require("../images/sdArch.jpeg");
+export const sbArch = require("../images/sbArch.jpeg");
