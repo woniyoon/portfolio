55f10cef55ab4aea11b68a7df2fa08d2eba59d04
woniyoon <gellifisch@gmail.com>
2020-10-07 23:59:27 +0900

    Wrap About inside HashRouter in App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import { HashRouter } from "react-router-dom";
 import Navigator from "./Components/Navigator";
+import About from "./Components/About";
 
 function App() {
   return (
     <HashRouter>
       <div className="entirePage">
         <Navigator />
+        <About />
       </div>
     </HashRouter>
   );
