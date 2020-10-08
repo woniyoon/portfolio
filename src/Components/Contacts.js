1513387c17f1ee505bb8e85dd65371c9f7e2aae9
woniyoon <gellifisch@gmail.com>
2020-10-08 23:20:13 +0900

    Add contacts methods

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -1,9 +1,19 @@
 import React from "react";
+import "./Contacts.css";
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 function Contacts(){
     return (
         <section id="contacts" className="contacts">
-            <h3>SNS</h3>
+            <h2>CONTACTS</h2>
+            <article className="contactsContainer">
+                <a href="mailto:woniyoon@gmail.com"><MailOutlineIcon fontSize="large"/></a>
+                <a href="https://www.instagram.com/yoonj1i/"><InstagramIcon fontSize="large" /></a>
+                <a href="https://github.com/woniyoon" target="_blank"><GitHubIcon fontSize="large" /></a>
+            </article>
+            <p>See you again!</p>
         </section>
     );
 }
